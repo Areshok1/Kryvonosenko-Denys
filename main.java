fe83ec39cf7de86a3df70c23c259dfa2c9d98257
Areshok1 <den.snaip@gmail.com>
2023-09-22 00:00:52 +0300

    Added a 'Library' class
    Added a 'Library' class with methods for managing a collection of 'Book' objects. The 'Library' class allows adding, displaying, searching, and removing books from the library.

--- main.java
+++ main.java
@@ -35,3 +35,48 @@ class Book {
         return "Title: " + title + ", Author: " + author + ", ISBN: " + isbn + ", Year: " + year;
     }
 }
+
+class Library {
+    private List<Book> books;
+
+    public Library() {
+        this.books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+        System.out.println("Book added to the library: " + book.getTitle());
+    }
+
+    public void showBooks() {
+        if (books.isEmpty()) {
+            System.out.println("The library is empty.");
+        } else {
+            System.out.println("Books in the library:");
+            for (Book book : books) {
+                System.out.println(book);
+            }
+        }
+    }
+
+    public void searchBookByTitle(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                System.out.println("Book found: " + book);
+                return;
+            }
+        }
+        System.out.println("Book with title '" + title + "' not found in the library.");
+    }
+
+    public void removeBookByIsbn(String isbn) {
+        for (Book book : books) {
+            if (book.getIsbn().equals(isbn)) {
+                books.remove(book);
+                System.out.println("Book removed from the library: " + book.getTitle());
+                return;
+            }
+        }
+        System.out.println("Book with ISBN '" + isbn + "' not found in the library.");
+    }
+}
